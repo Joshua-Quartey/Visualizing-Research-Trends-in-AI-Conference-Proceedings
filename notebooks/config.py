67709fbf527383d4467e7d130e662d8a3aa6e309
Joshua-Quartey <joshua.quartey@ucalgary.ca>
2025-02-14 13:17:43 +0000

    updated config.py

--- notebooks/config.py
+++ notebooks/config.py
@@ -4,6 +4,12 @@ def main():
   raw_data_path = "../data/raw/2016_2024.csv"
   raw_df = clean.pd.read_csv(raw_data_path)
 
+  clean_df = clean.clean_missing_values(raw_df)
+  clean_df = clean.clean_drop(clean_df)
+  clean_df = clean.types_conversion(clean_df)
+  clean_df = clean.impute_na(clean_df)
+  clean_df = clean.reformat_values(clean_df)
+  
   return raw_df, clean_df
 
 raw_df, clean_df = main()
